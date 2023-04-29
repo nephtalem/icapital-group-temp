09317a3fcbf3badfb1ed47d6646fe8bc0aa7db14
Besufkad Menji <besufkadmenji@gmail.com>
2023-04-29 03:10:28 +0300

    chore: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 git pull
 yarn install
 rm -rf .next
-pm2 stop shewaber
-pm2 delete shewaber
+pm2 stop icapital
+pm2 delete icapital
 rm .env
 echo '
-NEXT_PUBLIC_DOMAIN=shewaber.com
-NEXT_PUBLIC_API=https://api.shewaber.com/graphql
-NEXT_PUBLIC_DATA=https://api.shewaber.com
-NEXT_PUBLIC_SOCKET=wss://api.shewaber.com/graphql
+NEXT_PUBLIC_DOMAIN=icapital.com
+NEXT_PUBLIC_API=https://api.icapital.com/graphql
+NEXT_PUBLIC_DATA=https://api.icapital.com
+NEXT_PUBLIC_SOCKET=wss://api.icapital.com/graphql
 NEXT_PUBLIC_NODE_ENV=production
-APOLLO_KEY=service:Shewaber:x61JwV4PSS40Y7aI5kJnoQ
-APOLLO_GRAPH_REF=Shewaber@current
-APOLLO_SCHEMA_REPORTING=true
+
 ' >.env
 yarn build
-pm2 start yarn --name shewaber -- start
+pm2 start yarn --name icapital -- start
