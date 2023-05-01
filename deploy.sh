ad4d37740382842aba6014b514d2fdb63bc6967b
Besufkad Menji <besufkadmenji@gmail.com>
2023-05-01 03:48:40 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ pm2 delete icapital
 rm .env
 echo '
 NEXT_PUBLIC_DOMAIN=icapital.com
-NEXT_PUBLIC_API=https://api.icapital.com/graphql
-NEXT_PUBLIC_DATA=https://api.icapital.com
-NEXT_PUBLIC_SOCKET=wss://api.icapital.com/graphql
+NEXT_PUBLIC_API=https://icapital-cms.techawks.io/graphql
+NEXT_PUBLIC_DATA=https://icapital-cms.techawks.io
+NEXT_PUBLIC_SOCKET=wss://icapital-cms.techawks.io/graphql
 NEXT_PUBLIC_NODE_ENV=production
 
 ' >.env
