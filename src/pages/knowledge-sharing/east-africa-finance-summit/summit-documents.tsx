caefe9dddf70c94772d9cba6fe329443a515d8e5
Besufkad Menji <besufkadmenji@gmail.com>
2023-05-01 21:54:14 +0300

    fix

--- src/pages/knowledge-sharing/east-africa-finance-summit/summit-documents.tsx
+++ src/pages/knowledge-sharing/east-africa-finance-summit/summit-documents.tsx
@@ -2,6 +2,8 @@ import { Content } from "@/components/knowledge-sharing/Content";
 import { PlatformHeader } from "@/components/platform/PlatformHeader";
 import { SummitDocuments } from "@/components/platform/SummitDocuments";
 import { Title } from "@/components/shared/Title";
+import KSPService from "@/services/ksp.service";
+import { GetStaticProps } from "next";
 
 const SummitDocumentsEAFSPage = () => {
   return (
@@ -16,3 +18,14 @@ const SummitDocumentsEAFSPage = () => {
 };
 
 export default SummitDocumentsEAFSPage;
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const upcomingEafs = await KSPService.upcomingEafs();
+  return {
+    props: {
+      upcomingEafs,
+    },
+    revalidate: 10, // In seconds
+  };
+};
