63472f634a47a35bfdc848e304a402ff4cc995e5
Besufkad Menji <besufkadmenji@gmail.com>
2023-05-08 20:10:26 +0300

    chore: deploy

---
+++ build.sh
@@ -0,0 +1,13 @@
+yarn install
+rm -rf .next
+rm .env
+echo '
+NEXT_PUBLIC_DOMAIN=icapital.com
+NEXT_PUBLIC_API=https://icapital-cms.techawks.io/graphql
+NEXT_PUBLIC_DATA=https://icapital-cms.techawks.io
+NEXT_PUBLIC_SOCKET=wss://icapital-cms.techawks.io/graphql
+NEXT_PUBLIC_NODE_ENV=production
+
+' >.env
+
+yarn build
