fa5fae8554c4a36bef139084e5497f7993b3a55a
Besufkad Menji <besufkadmenji@gmail.com>
2023-12-04 01:35:09 +0300

    fix

--- src/components/blogs/Blogs.tsx
+++ src/components/blogs/Blogs.tsx
@@ -3,7 +3,7 @@ import { Blog } from "./Blog";
 
 export const Blogs = ({ blogs }: { blogs: BlogEntity[] }) => {
   return (
-    <div className="mx-[2.5rem] mb-[10rem] grid grid-cols-2">
+    <div className="mx-[2.5rem] gap-10 mb-[10rem] grid grid-cols-2">
       {blogs.map((blog, index) => (
         <Blog blog={blog} key={index} />
       ))}
